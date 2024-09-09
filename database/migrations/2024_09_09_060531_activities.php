640b377b89f386ab2c6bfb961da99f8184b1839e
Marion-Brownies <koromoro286@gmail.com>
2024-09-09 13:26:28 +0700

    add new table

---
+++ database/migrations/2024_09_09_060531_activities.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->interger('teacher_id');
+            $table->interger('category_id');
+            $table->text('activity');
+            $table->date('date_start');
+            $table->string('updated_by')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('deleted_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
