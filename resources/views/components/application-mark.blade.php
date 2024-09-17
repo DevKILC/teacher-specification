26b77e456ac23ae6f7bcd9bd403e606df4d199e5
Marion-Brownies <koromoro286@gmail.com>
2024-09-17 20:06:40 +0700

    logo sementara

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,2 +1,4 @@
+<!-- 
+<img src="{{ asset('img/lclogonobg.png') }}" class="w-[150px] h-[60px]"  alt=""  {{ $attributes }}> -->
 
-<img src="{{ asset('img/lclogonobg.png') }}" class="w-[150px] h-[60px]"  alt=""  {{ $attributes }}>
+<img src="{{ asset('img/teacher.png') }}" class="w-[150px] h-[60px]"  alt=""  {{ $attributes }}>
