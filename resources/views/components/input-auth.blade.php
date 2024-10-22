fe1e97b70eb66673d5351ed20b8dab7d9b48810c
Marion-Brownies <koromoro286@gmail.com>
2024-10-22 12:42:04 +0700

    feat : update-input component

--- resources/views/components/input-auth.blade.php
+++ resources/views/components/input-auth.blade.php
@@ -1,6 +1,4 @@
 @props(['disabled' => false])
 
 <input {{ $disabled ? 'disabled' : '' }} 
-    x-data 
-    x-on:input="$event.target.value = $event.target.value.toUpperCase()"
     {!! $attributes->merge(['class' => 'border-gray-300 focus:border-yellow-500 focus:ring-yellow-500 rounded-md shadow-sm']) !!}>
